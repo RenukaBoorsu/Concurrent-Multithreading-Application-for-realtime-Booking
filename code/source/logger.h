70630b7f6fcbcbe3d33c61e843809854311ba598
Keerthi-200 <116144550+Keerthi-200@users.noreply.github.com>
2022-11-23 17:33:35 +0530

    Delete logger.h

--- code/source/logger.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdarg.h>
-#include <time.h>
-#include <string.h>
-#include <stdlib.h>
-void log_print(char* filename,char* func, int line, char *fmt,...);
-#define LOG_PRINT(...) log_print(__FILE__,__func__, __LINE__, __VA_ARGS__ )
-#define LOG_INFO(message, args...)      LOG_PRINT(LOG_FMT message NEWLINE, ## args)
