ef5fc98e03e175f5b2d9189cc326887187f86fc4
lasya <lasyas762@gmail.com>
2022-11-25 13:05:19 +0530

    server files

---
+++ code/SRC/Server/server_header.h
@@ -0,0 +1,16 @@
+#include <netinet/in.h>
+#include <cstring>
+#include <stdio.h>
+#include <algorithm>
+#include <pthread.h>
+#include <signal.h>
+#include <iostream>
+#include<netdb.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+
+using namespace std;
+
